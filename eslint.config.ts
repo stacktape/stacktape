51c10c1416dbc18f44c9e6718e34a303f695c2d8
Matúš <matus.congrady@stacktape.com>
2026-02-25 18:31:40 +0100

    chore: move cspell jargon to dedicated dictionary file

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,5 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import antfu from '@antfu/eslint-config';
-import cspellPlugin from '@cspell/eslint-plugin';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default antfu(
   {
@@ -19,7 +13,7 @@ export default antfu(
       '__publish-folder/*',
       '__binary-dist/*',
       'docs/next-env.d.ts',
-      '**/*.md',
+      '**/*.md'
     ],
     regexp: false,
     lessOpinionated: true,
@@ -30,26 +24,11 @@ export default antfu(
         'react-hooks-extra/no-direct-set-state-in-use-effect': ['off'],
         'react-refresh/only-export-components': ['off'],
         'react/no-array-index-key': ['off'],
-        'react-hooks/set-state-in-effect': ['off'],
-      },
+        'react-hooks/set-state-in-effect': ['off']
+      }
     },
     yaml: true,
-    plugins: {
-      '@cspell': cspellPlugin,
-    },
     rules: {
-      '@cspell/spellchecker': [
-        'warn',
-        {
-          configFile: join(__dirname, 'cspell.config.json'),
-          checkComments: true,
-          checkStringTemplates: true,
-          checkStrings: true,
-          numSuggestions: 3,
-          checkIdentifiers: true,
-          autoFix: true,
-        },
-      ],
       'unicorn/prefer-number-properties': 'off',
       'perfectionist/sort-named-imports': 'off',
       'import/consistent-type-specifier-style': 'off',
@@ -64,22 +43,22 @@ export default antfu(
         {
           argsIgnorePattern: '^_',
           varsIgnorePattern: '^_',
-          args: 'after-used',
-        },
+          args: 'after-used'
+        }
       ],
       'antfu/consistent-list-newline': 'off',
       'ts/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
       'n/prefer-global/process': ['error', 'always'],
       'dot-notation': ['off'],
-      'no-case-declarations': ['off'],
+      'no-case-declarations': ['off']
     },
     typescript: {
       overrides: {
         'ts/no-unused-expressions': ['off'],
         'ts/consistent-type-definitions': ['off'],
-        'ts/no-use-before-define': ['off'],
-      },
-    },
+        'ts/no-use-before-define': ['off']
+      }
+    }
   },
-  eslintPluginPrettier,
+  eslintPluginPrettier
 );
